b221507360e1728aed6544181a06a72028537a57
Kumar <824985@cognizant.com>
2019-11-28 14:11:23 +0800

    added store for git_interface

---
+++ git_interface/src/Store/reducers/githubData.js
@@ -0,0 +1,23 @@
+// state - storing the array of objects from git api
+const initialState = {
+  data: [{}]
+};
+
+// if actions are invoked in the components, the state is updated accordingly
+const reducer = (state = initialState, action) => {
+  switch (
+    action.type // if the action type matches (as mentioned within the component)
+  ) {
+    case action.SAVE_DATA: {
+      return {
+        ...state,
+        data: action.value // action.value is the array obtained from github api
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
